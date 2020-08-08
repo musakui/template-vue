ffa0dd5ae1983e61d26ee96f36e4903f2dad1390
Shun Git <kwokshungit@gmail.com>
2020-08-08 18:02:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<button @click="count++">count is {{ count }}</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const count = ref(0)
+
+    return {
+      count,
+    }
+  }
+}
+</script>
